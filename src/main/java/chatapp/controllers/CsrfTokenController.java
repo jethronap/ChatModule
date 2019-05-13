62e2241c4c68bd3d325169ea81f753afad3d7c0f
jethronap <efthioa@gmail.com>
2019-05-13 22:14:40 +0300

    added controllers

---
+++ src/main/java/chatapp/controllers/CsrfTokenController.java
@@ -0,0 +1,22 @@
+package chatapp.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author jnap
+ */
+@Controller
+public class CsrfTokenController {
+
+    @GetMapping("/csrf")
+    public @ResponseBody
+    String getCsrfToken(HttpServletRequest request) {
+        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        return csrf.getToken();
+    }
+}
---
+++ src/main/java/chatapp/controllers/SocketController.java
@@ -0,0 +1,42 @@
+package chatapp.controllers;
+
+import java.security.Principal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author jnap
+ */
+@Controller
+public class SocketController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private static final Logger log = LoggerFactory.getLogger(SocketController.class);
+
+    @MessageMapping(SECURED_CHAT)
+    @SendTo(SECURED_CHAT_HISTORY)
+    public OutputMessage sendAll(Message msg) throws Exception {
+        OutputMessage out = new OutputMessage(msg.getFrom(), msg.getText(), new SimpleDateFormat("HH:mm").format(new Date()));
+        return out;
+    }
+
+    /**
+     * Example of sending message to specific user using 'convertAndSendToUser()' and '/queue'
+     */
+    @MessageMapping(SECURED_CHAT_ROOM)
+    public void sendSpecific(@Payload Message msg, Principal user, @Header("simpSessionId") String sessionId) throws Exception {
+        OutputMessage out = new OutputMessage(msg.getFrom(), msg.getText(), new SimpleDateFormat("HH:mm").format(new Date()));
+        simpMessagingTemplate.convertAndSendToUser(msg.getTo(), SECURED_CHAT_SPECIFIC_USER, out);
+    }    
+}
