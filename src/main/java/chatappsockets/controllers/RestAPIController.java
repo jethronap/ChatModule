5207c2f43067cde7854b06445085a1028bafd158
jethronap <efthioa@gmail.com>
2019-05-13 22:49:49 +0300

    added controllers

---
+++ src/main/java/chatappsockets/controllers/RestAPIController.java
@@ -0,0 +1,21 @@
+package chatappsockets.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jnap
+ */
+@RestController
+@RequestMapping(path = "/rest")
+public class RestAPIController {
+
+    @GetMapping(path = "/{name}", produces = "application/json")
+    public String getGreeting(@PathVariable("name") String name) {
+        return "{\"greeting\" : \"Hello, " + name + "!\"}";
+    }
+    
+}
---
+++ src/main/java/chatappsockets/controllers/WebSocketController.java
@@ -0,0 +1,21 @@
+package chatappsockets.controllers;
+
+import chatappsecure.transfer.socket.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ *
+ * @author jnap
+ */
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(Message message) throws Exception {
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }    
+}
